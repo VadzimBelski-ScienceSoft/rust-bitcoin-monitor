b6d6cbc1f6f601997a4ee20d9ea39e225a6f2c07
ybely <ybely@scnsoft.com>
2022-09-06 13:37:39 +0300

    styling accorting linter

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,44 @@
+extern crate bitcoin;
 extern crate bitcoincore_rpc;
+extern crate jsonrpc;
+extern crate log;
 extern crate serde_json;
 extern crate ticker;
-extern crate log;
-extern crate jsonrpc;
 extern crate tokio;
-extern crate bitcoin;
 
+use bitcoincore_rpc::{Auth, Client, RpcApi};
 use std::time::Duration;
-use bitcoincore_rpc::{Client, Auth, RpcApi};
 use ticker::Ticker;
 
 use warp::Filter;
 
 use std::str::FromStr;
 
-use bitcoin::{Address};
 use bip0039::{Count, Mnemonic};
 use bitcoin::secp256k1::ffi::types::AlignedType;
 use bitcoin::secp256k1::Secp256k1;
 use bitcoin::util::bip32::{ChildNumber, DerivationPath, ExtendedPrivKey, ExtendedPubKey};
+use bitcoin::Address;
 use bitcoin::PublicKey;
 
 pub struct WebServer;
 
 impl WebServer {
-
     pub async fn run() {
-
         println!("Starting webserver...");
 
         // GET /hello/warp => 200 OK with body "Hello, warp!"
-        let add_address = warp::path!("address")
-            .map(|| {
-               let address = generate_address();
-               warp::reply::json(&address)
-            });
-    
+        let add_address = warp::path!("address").map(|| {
+            let address = generate_address();
+            warp::reply::json(&address)
+        });
+
         warp::serve(add_address).bind(([127, 0, 0, 1], 3030)).await;
-
     }
-
 }
 
 #[tokio::main]
 async fn main() {
-
     env_logger::init();
 
     // run webserver
@@ -54,24 +48,25 @@ async fn main() {
     });
     println!("Everything working good!");
 
-    let rpc = Client::new("http://127.0.0.1:10003",
-                          Auth::UserPass("user".to_string(),
-                                         "user".to_string())).unwrap();
+    let rpc = Client::new(
+        "http://127.0.0.1:10003",
+        Auth::UserPass("user".to_string(), "user".to_string()),
+    )
+    .unwrap();
 
     let ticker = Ticker::new(0.., Duration::from_secs(5));
 
     let block_count = rpc.get_block_count().unwrap();
     println!("block count: {}", block_count);
-        
+
     let mut next_block = rpc.get_block_hash(block_count).unwrap();
-    let mut latest_scanned_block : String = "".to_string();
+    let mut latest_scanned_block: String = "".to_string();
 
     for _ in ticker {
- 
         println!("We are on the block {}", next_block);
 
         let block = rpc.get_block_info(&next_block).unwrap();
-        
+
         if latest_scanned_block != block.hash.to_string() {
             for tx in &block.tx {
                 scan_transaction(&tx, &rpc);
@@ -79,23 +74,23 @@ async fn main() {
             }
         }
         // Lets got to next transaction only after 2 confirmations ?
-        if  block.nextblockhash != None && block.confirmations >= 2 {
+        if block.nextblockhash != None && block.confirmations >= 2 {
             println!("{}", serde_json::to_string_pretty(&block).unwrap());
             next_block = block.nextblockhash.unwrap();
-        }else{
+        } else {
             println!("No more blocks");
-        }            
-    
+        }
     }
-
 }
 
 fn scan_transaction(tx: &bitcoincore_rpc::bitcoin::Txid, rpc: &bitcoincore_rpc::Client) {
-
     let raw_transaction_hex = rpc.get_raw_transaction_hex(&tx, None).unwrap();
-    let args = [serde_json::to_value(&raw_transaction_hex).unwrap(), true.into()];
+    let args = [
+        serde_json::to_value(&raw_transaction_hex).unwrap(),
+        true.into(),
+    ];
 
-    let result : serde_json::Value =  rpc.call("decoderawtransaction", &args ).unwrap();
+    let result: serde_json::Value = rpc.call("decoderawtransaction", &args).unwrap();
     let json_object = result.as_object().unwrap();
 
     for index in json_object["vout"].as_array().unwrap() {
@@ -104,7 +99,6 @@ fn scan_transaction(tx: &bitcoincore_rpc::bitcoin::Txid, rpc: &bitcoincore_rpc::
 }
 
 fn generate_address() -> String {
-
     let network = bitcoin::Network::Bitcoin;
 
     // Generates an English mnemonic with 12 words randomly
@@ -137,7 +131,10 @@ fn generate_address() -> String {
     // generate first receiving address at m/0/0
     // manually creating indexes this time
     let zero = ChildNumber::from_normal_idx(0).unwrap();
-    let public_key = xpub.derive_pub(&secp, &vec![zero, zero]).unwrap().public_key;
+    let public_key = xpub
+        .derive_pub(&secp, &vec![zero, zero])
+        .unwrap()
+        .public_key;
     let address = Address::p2wpkh(&PublicKey::new(public_key), network).unwrap();
     println!("First receiving address: {}", address);
 
